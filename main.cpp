691756646f3397daa733839662bd80b48e5c7037
Maciej Korbaś <maciej.korbas@dsnet.pl>
2023-01-23 19:46:03 +0100

    Poprawka tekstury

--- main.cpp
+++ main.cpp
@@ -109,11 +109,11 @@ int main()
     -0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // bottom-right
     -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // top-right
     // Right face
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
+     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right
      0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // top-right         
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // bottom-right
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // top-left
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // bottom-right
+     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // top-left
      0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // bottom-left     
     // Bottom face
     -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // top-right
